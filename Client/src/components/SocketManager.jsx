3c62535010da341041ff682b0b642c07fcb21084
SuperKeith333 <115736211+SuperKeith333@users.noreply.github.com>
2024-03-30 12:54:18 +0000

    client players

--- Client/src/components/SocketManager.jsx
+++ Client/src/components/SocketManager.jsx
@@ -1,9 +1,13 @@
 import { useEffect } from "react";
 import { io} from "socket.io-client"
+import { useAtom, atom} from "jotai"
 
 export const socket = io("https://scaling-garbanzo-p4vvw4g977536gg-3001.app.github.dev");
+export const charactersAtom = atom([]);
+
 
 export const SocketManager = () => {
+    const [_characters, setCharacters] = useAtom(charactersAtom);
     useEffect(() => {
         function onConnect(){
             console.log("connected");
@@ -16,14 +20,21 @@ export const SocketManager = () => {
             console.log("hello");
         }
 
+        function onCharaters(value){
+            setCharacters(value);
+            console.log(value);
+        }
+
         socket.on("connect", onConnect);
         socket.on("disconnect", onDisconnect);
         socket.on("hello", onHello);
+        socket.on("characters", onCharaters);
 
         return () => {
             socket.off("connect", onConnect);
             socket.off("disconnect", onDisconnect);
             socket.off("hello", onHello);
+            socket.off("characters", onCharaters);
         }
 
     }, [])
--- Server/index.js
+++ Server/index.js
@@ -8,13 +8,39 @@ const io = new Server({
 
 io.listen(3001);
 
+const characters = [];
+
+const generateRandomPosition = () =>{
+    return [Math.random() * 3, 0, Math.random() * 3];
+};
+
+const generateRandomHexColor = () => {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+};
+
+
 io.on("connection", (Socket) => {
     console.log("user connected");
 
+    characters.push({
+        id: Socket.id,
+        position: generateRandomPosition(),
+        hairColor: generateRandomHexColor(),
+        topColor: generateRandomHexColor(),
+        bottomColor: generateRandomHexColor()
+    });
+
     Socket.emit("hello");
 
-    Socket.on("disconnect", () => {
+    io.emit("characters", characters);
 
+    Socket.on("disconnect", () => {
         console.log("user disconnected");
+
+        characters.splice(
+            characters.findIndex((character) => character.id === Socket.id)
+        );
+
+        io.emit("characters", characters);
     })
 });
